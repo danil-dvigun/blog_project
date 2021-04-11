ba179574cb75fce337c6d34a9962e105170876c8
danil-dvigun <dvigundanil@gmail.com>
2021-04-11 20:29:17 +0300

    Test first controller 2
    Added home route 2

--- server_api/src/Controller/HomeController.php
+++ server_api/src/Controller/HomeController.php
@@ -1,4 +1,20 @@
+<?php
+namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class HomeController{
+    /**
+     * @Route("/", name="blog_list", methods={"GET"})
+     */
+    public function getNumber(): Response
+    {
+        $number = 23;
 
-class Home
+        $response = new Response();
+        $response->setContent(json_encode(["name"=>"Danil", "age"=>$number]));
+        return $response;
+
+    }
+}
