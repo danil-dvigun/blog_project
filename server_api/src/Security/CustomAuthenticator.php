1b9e982a591c31f04733f48cba331a97b4ac251b
Danil <dvigundanil@gmail.com>
2021-04-13 17:32:47 +0300

    added auth class

---
+++ server_api/src/Security/CustomAuthenticator.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+class CustomAuthenticator implements AuthenticationEntryPointInterface
+{
+    public function supports(Request $request)
+    {
+        echo "test";
+        // todo
+    }
+
+    public function getCredentials(Request $request)
+    {
+        // todo
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        // todo
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // todo
+    }
+
+    public function createAuthenticatedToken(UserInterface $user, string $providerKey)
+    {
+        // todo
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        // todo
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
+    {
+        // todo
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        // todo
+    }
+
+    public function supportsRememberMe()
+    {
+        // todo
+    }
+}
