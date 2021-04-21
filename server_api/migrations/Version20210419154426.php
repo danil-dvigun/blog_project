83f8c13193f7287531a8cc54d612d351e5d06f4e
Danil <dvigundanil@gmail.com>
2021-04-21 17:59:45 +0300

    Routing in react
    Added routing and first version cards

--- server_api/migrations/Version20210419154426.php
+++ server_api/migrations/Version20210419154426.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210419123425 extends AbstractMigration
+final class Version20210419154426 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,6 +20,7 @@ final class Version20210419123425 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE post_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE post (id INT NOT NULL, owner_id INT NOT NULL, title VARCHAR(255) NOT NULL, discription VARCHAR(255) DEFAULT NULL, text TEXT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_5A8A6C8D7E3C61F9 ON post (owner_id)');
         $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, salt VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
@@ -32,6 +33,7 @@ final class Version20210419123425 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE post DROP CONSTRAINT FK_5A8A6C8D7E3C61F9');
+        $this->addSql('DROP SEQUENCE post_id_seq CASCADE');
         $this->addSql('DROP TABLE post');
         $this->addSql('DROP TABLE "user"');
     }
